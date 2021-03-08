02dce30eb827ae6e42d4c450e7dfc1d00c63d128
millenium-dev <55107812+millenium-dev@users.noreply.github.com>
2021-03-09 00:41:06 +0100

    Update db.py

--- db.py
+++ db.py
@@ -3,6 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:qwerty123@localhost"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://foxvipfhigsqsn:0a29a72342fbe018656388d286131ab750294f19e1d5a2af4e1a6daedcf281f5@ec2-54-163-140-104.compute-1.amazonaws.com:5432/d11bng82tkrb0c"
 db = SQLAlchemy(app)
